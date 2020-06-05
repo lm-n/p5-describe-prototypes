d605b682ffeb7101bc459e9b7f7ad56aaa58ec48
Luis Morales-Navarro <lmn297@nyu.edu>
2020-06-05 08:17:23 -0600

    update desc

--- describeElement/example2/desc.js
+++ describeElement/example2/desc.js
@@ -1,57 +1,51 @@
-const FALLBACK = 0, LABEL = 1;
-
-function describe(text, display) {
-  if (document.getElementById('canvasDescription') == undefined) {
-    document.getElementById('defaultCanvas0').innerHTML = '<div id="canvasDescription" role="region" aria-label="Canvas Description"><p id="description"></p></div>';
-  } else if (document.getElementById('description') == undefined) {
-    document.getElementById('elementsDescription').insertAdjacentHTML('beforebegin', '<p id="description"></p>');
+const FALLBACK = 0, LABEL = 1, canvasID = 'defaultCanvas0';
+function describe(text,display) {
+  if (document.getElementById(canvasID+'Description') == undefined) {
+    document.getElementById(canvasID).innerHTML = '<div id="'+canvasID+'Description" role="region" aria-label="Canvas Description"><p id="'+canvasID+'desc"></p></div>';
+  } else if (document.getElementById(canvasID+'desc') == undefined) {
+    document.getElementById(canvasID+'elDes').insertAdjacentHTML('beforebegin', '<p id="'+canvasID+'desc"></p>');
   }
-  if (document.getElementById('description').innerHTML != text) {
-    document.getElementById('description').innerHTML = text;
+  if (document.getElementById(canvasID+'desc').innerHTML != text) {
+    document.getElementById(canvasID+'desc').innerHTML = text;
   }
   if (display == 1) {
-    if (document.getElementById('canvasLabel') == undefined) {
-      document.getElementById('defaultCanvas0').insertAdjacentHTML('afterend', '<div id="canvasLabel"><p id=dLbl></p></div>');
-    } else if (document.getElementById('dLbl') == undefined) {
-      document.getElementById('elementsLbl').insertAdjacentHTML('beforebegin', '<p id=dLbl></p>');
+    if (document.getElementById(canvasID+'Label') == undefined) {
+      document.getElementById(canvasID).insertAdjacentHTML('afterend', '<div id="'+canvasID+'Label"><p id='+canvasID+'dLbl></p></div>');
+    } else if (document.getElementById(canvasID+'dLbl') == undefined) {
+      document.getElementById(canvasID+'eLbl').insertAdjacentHTML('beforebegin', '<p id='+canvasID+'dLbl></p>');
     }
-    if (document.getElementById('dLbl').innerHTML != text) {
-      document.getElementById('dLbl').innerHTML = text;
+    if (document.getElementById(canvasID+'dLbl').innerHTML != text) {
+      document.getElementById(canvasID+'dLbl').innerHTML = text;
     }
   }
 }
-
 function describeElement(name, text, display) {
-  if (document.getElementById('canvasDescription') == undefined) {
-    document.getElementById('defaultCanvas0').innerHTML = '<div id="canvasDescription" role="region" aria-label="Canvas Description"><table id="elementsDescription"></table></div>';
-  } else if (document.getElementById('elementsDescription') == undefined) {
-    document.getElementById('description').insertAdjacentHTML('afterend', '<table id="elementsDescription"></table>');
+  if (document.getElementById(canvasID+'Description') == undefined) {
+    document.getElementById(canvasID).innerHTML = '<div id="'+canvasID+'Description" role="region" aria-label="Canvas Description"><table id="'+canvasID+'elDes"></table></div>';
+  } else if (document.getElementById(canvasID+'elDes') == undefined) {
+    document.getElementById(canvasID+'desc').insertAdjacentHTML('afterend', '<table id="'+canvasID+'elDes"></table>');
   }
-  if (document.getElementById(name) == undefined) {
+  if (document.getElementById(canvasID+name) == undefined) {
     let tr = document.createElement("tr");
-    tr.id = name;
-    document.getElementById('elementsDescription').appendChild(tr);
+    tr.id = canvasID+name;
+    document.getElementById(canvasID+'elDes').appendChild(tr);
   }
-  if (document.getElementById(name).innerHTML != '<td>' + name + '</td><td>' + text + '</td>') {
-    document.getElementById(name).innerHTML = '<td>' + name + '</td><td>' + text + '</td>';
+  if (document.getElementById(canvasID+name).innerHTML != '<td>' + name + '</td><td>' + text + '</td>') {
+    document.getElementById(canvasID+name).innerHTML = '<td>' + name + '</td><td>' + text + '</td>';
   }
   if (display == 1) {
-    displayx(name, text);
-  }
-}
-
-function displayx(name, text) {
-  if (document.getElementById('canvasLabel') == undefined) {
-    document.getElementById('defaultCanvas0').insertAdjacentHTML('afterend', '<div id="canvasLabel"><table id="elementsLbl"></table></div>');
-  } else if (document.getElementById('elementsLbl') == undefined) {
-    document.getElementById('dLbl').insertAdjacentHTML('afterend', '<table id="elementsLbl"></table>');
-  }
-  if (document.getElementById(name + 'Lbl') == undefined) {
-    let tr = document.createElement("tr");
-    tr.id = name + 'Lbl';
-    document.getElementById('elementsLbl').appendChild(tr);
-  }
-  if (document.getElementById(name + 'Lbl').innerHTML != '<td>' + name + '</td><td>' + text + '</td>') {
-    document.getElementById(name + 'Lbl').innerHTML = '<td>' + name + '</td><td>' + text + '</td>';
+    if (document.getElementById(canvasID+'Label') == undefined) {
+      document.getElementById(canvasID).insertAdjacentHTML('afterend', '<div id="'+canvasID+'Label"><table id="'+canvasID+'eLbl"></table></div>');
+    } else if (document.getElementById(canvasID+'eLbl') == undefined) {
+      document.getElementById(canvasID+'dLbl').insertAdjacentHTML('afterend', '<table id="'+canvasID+'eLbl"></table>');
+    }
+    if (document.getElementById(canvasID+name + 'Lbl') == undefined) {
+      let tr = document.createElement("tr");
+      tr.id = canvasID+name + 'Lbl';
+      document.getElementById(canvasID+'eLbl').appendChild(tr);
+    }
+    if (document.getElementById(canvasID+name + 'Lbl').innerHTML != '<td>' + name + '</td><td>' + text + '</td>') {
+      document.getElementById(canvasID+name + 'Lbl').innerHTML = '<td>' + name + '</td><td>' + text + '</td>';
+    }
   }
 }
